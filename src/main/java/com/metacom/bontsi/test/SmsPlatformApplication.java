4f0c00e516e175645acad7a0c62ee50685a22c7b
ndimphiwe.bontsi <ndimphiwe.bontsi@metacom.co.za>
2018-11-06 16:27:45 +0200

    first commit

---
+++ src/main/java/com/metacom/bontsi/test/SmsPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.metacom.bontsi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsPlatformApplication.class, args);
+	}
+}
